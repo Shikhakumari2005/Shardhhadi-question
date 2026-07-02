fce14946cece319123c30dec0ba9f9055276e740
Your Name <siragenius1@gmail.com>
2026-07-02 13:05:42 +0530

    MaxWidthOFCBT

--- binary tree/maximumwidthbt.java
+++ binary tree/maximumwidthbt.java
@@ -9,39 +9,81 @@ class TreeNode{
         this.val=val;
     }
 }
+class Pair {
+    TreeNode node;
+    long index;
+
+    Pair(TreeNode node, long index) {
+        this.node = node;
+        this.index = index;
+    }
+}
 class maximumwidthbt {
-    public int MaxWidth(TreeNode a){
-        int max=Integer.MIN_VALUE;
-        if(a==null) return 0;
-        Queue<TreeNode> q=new LinkedList<>();
-        //int count=0;
-        q.offer(a);
-        while(!q.isEmpty()){
-            int cursize=q.size();
-            // count++;
-            max=Math.max(cursize,max);
-            //System.out.println(cur);
-            for (int i = 0; i < cursize; i++) {
-                TreeNode cur=q.poll();
-                if(cur.left!=null){
-                    q.offer(cur.left);
-                }
-                if(cur.right!=null){
-                    q.offer(cur.right);
-                }
+    public int MaxWidth(TreeNode root){
+        // int max=Integer.MIN_VALUE;
+        // if(a==null) return 0;
+        // Queue<TreeNode> q=new LinkedList<>();
+        // q.offer(a);
+        // while(!q.isEmpty()){
+        //     int cursize=q.size();
+        //     max=Math.max(cursize,max);
+        //     for (int i = 0; i < cursize; i++) {
+        //         TreeNode cur=q.poll();
+        //         if(cur.left!=null){
+        //             q.offer(cur.left);
+        //         }
+        //         if(cur.right!=null){
+        //             q.offer(cur.right);
+        //         }
+        //     }
+        // }
+        //return max;
+
+
+
+        if (root == null)
+            return 0;
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(root, 1));
+
+        int ans = 0;
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            long first = q.peek().index;
+            long last = first;
+
+            for (int i = 0; i < size; i++) {
+
+                Pair cur = q.poll();
+
+                last = cur.index;
+
+                if (cur.node.left != null)
+                    q.offer(new Pair(cur.node.left, cur.index * 2));
+
+                if (cur.node.right != null)
+                    q.offer(new Pair(cur.node.right, cur.index * 2 + 1));
             }
+
+            ans = Math.max(ans, (int)(last - first + 1));
         }
-        return max;
+
+        return ans;
     }
     public static void main(String[] args) {
-        TreeNode a=new TreeNode(1);
-        a.left=new TreeNode(2);
-        a.right=new TreeNode(3);
-        a.left.left=new TreeNode(5);
-        a.left.right=new TreeNode(6);
-        a.right.right=new TreeNode(7);
+        TreeNode root=new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(5);
+        root.left.right=new TreeNode(6);
+        root.right.right=new TreeNode(7);
         maximumwidthbt obj=new maximumwidthbt();
-        int ans=obj.MaxWidth(a);
+        int ans=obj.MaxWidth(root);
         System.out.print(ans);
 
     }
